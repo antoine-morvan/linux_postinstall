1af6de59b25f25cda6ae42425239312fbc19c3fd
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-03-06 16:43:22 +0100

    fix geany plugin path

--- 02_apps/geany/ubuntu.sh
+++ 02_apps/geany/ubuntu.sh
@@ -15,6 +15,8 @@ install_packs "$PKGS"
 mkdir -p /etc/skel/.config/geany/
 retry "wget -q -O /etc/skel/.config/geany/geany.conf ${SETUP_SCRIPT_LOCATION}/02_apps/geany/geany.conf"
 
+#different path for ubuntu
+sed -i -e 's#/usr/lib/geany/#/usr/lib/x86_64-linux-gnu/geany/#g' /etc/skel/.config/geany/geany.conf
 
 git clone https://github.com/codebrainz/geany-themes.git geany-themes
 cp -R geany-themes/colorschemes /usr/share/geany/
