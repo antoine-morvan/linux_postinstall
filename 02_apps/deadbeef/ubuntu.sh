c47e26e947ed90f3ca9accd8950c4f7023ddae95
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-03-06 13:50:59 +0100

    update deadbeef setup

--- 02_apps/deadbeef/ubuntu.sh
+++ 02_apps/deadbeef/ubuntu.sh
@@ -6,17 +6,16 @@ export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
 [ ! -e ubuntu_func.sh ] &&  wget -q ${SETUP_SCRIPT_LOCATION}/01_func/ubuntu_func.sh -O ubuntu_func.sh
 source ubuntu_func.sh
 
-
-
 add-apt-repository -y ppa:starws-box/deadbeef-player
+
 upgrade
 install_packs deadbeef
 
 mkdir -p /etc/skel/.config/deadbeef/
+retry "wget -q -O /etc/skel/.config/deadbeef/config ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/config"
 
-
-wget ${SETUP_SCRIPT_LOCATION}/ddb_misc_filebrowser_GTK2.so -O /usr/lib/deadbeef/ddb_misc_filebrowser_GTK2.so
-
+#add file browser plugin
+wget ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/ddb_misc_filebrowser_GTK2.so -O /usr/lib/deadbeef/ddb_misc_filebrowser_GTK2.so
 
 
 exit
