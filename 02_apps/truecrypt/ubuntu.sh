d6b4f8f6f56e7acec7b622a644b1f6482c569d54
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-03-06 10:46:32 +0100

    add sample script for veracrypt on ubuntu

---
+++ 02_apps/truecrypt/ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#https://launchpad.net/~unit193/+archive/ubuntu/encryption
+
+add-apt-repository -y ppa:unit193/encryption
+apt-get update
+apt-get -y upgrade
+apt-get -y install veracrypt
+
+exit 0
+
