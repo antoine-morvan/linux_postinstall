003ae56f1158eace8cbe1571f4845d6eaffdd1c2
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-03-06 12:38:03 +0100

    add conky for ubuntu

---
+++ 02_apps/conky/ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#configure script variables
+[ `whoami` != root ] && echo "should run as root" && exit 1
+export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
+[ ! -e ubuntu_func.sh ] &&  wget -q ${SETUP_SCRIPT_LOCATION}/01_func/ubuntu_func.sh -O ubuntu_func.sh
+source ubuntu_func.sh
+
+#setup application
+
+echo ""
+echo "** Setup Conky"
+echo ""
+
+upgrade
+install_packs conky-all
+
+dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/conky/conky_config.sh
