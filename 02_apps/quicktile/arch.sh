668371490ba17c51894d6614f4bc3b608b9c512a
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-03-06 10:53:59 +0100

    make arch setup use config file

--- 02_apps/quicktile/arch.sh
+++ 02_apps/quicktile/arch.sh
@@ -13,47 +13,10 @@ install_packs_aur "$AURPKGS"
 
 
 mkdir -p /etc/skel/.config/
-cat > /etc/skel/.config/quicktile.cfg << EOF
-[general]
-cfg_schema = 1
-UseWorkarea = True
-ModMask = <Mod4>
-
-[keys]
-space = middle
-H = horizontal-maximize
-V = vertical-maximize
-KP_0 = maximize
-KP_1 = bottom-left
-KP_2 = bottom
-KP_3 = bottom-right
-KP_4 = left
-KP_5 = middle
-KP_6 = right
-KP_7 = top-left
-KP_8 = top
-KP_9 = top-right
-Return = monitor-switch
-Up = top
-Down = bottom
-Left = left
-Right = right
-
-EOF
-
-
 mkdir -p /etc/xdg/autostart/
-cat > /etc/xdg/autostart/quicktile.desktop << EOF
-[Desktop Entry]
-Encoding=UTF-8
-Version=1.0
-Name=QuickTile
-GenericName=Window-Tiling Helper
-Type=Application
-Exec=quicktile --daemonize
-Categories=Utility;
 
-EOF
+retry "wget -q -O /etc/skel/.config/quicktile.cfg ${SETUP_SCRIPT_LOCATION}/02_apps/quicktile/quicktile.cfg"
+retry "wget -q -O /etc/xdg/autostart/quicktile.desktop ${SETUP_SCRIPT_LOCATION}/02_apps/quicktile/quicktile.desktop"
 chmod +x /etc/xdg/autostart/quicktile.desktop
 
 exit
