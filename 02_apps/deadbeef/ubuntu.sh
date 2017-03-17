04066c699cfc6d8df23e06bbba1125d98b287eee
Antoine Morvan <antoine-morvan@users.noreply.github.com>
2017-03-17 10:19:05 +0100

    Update ubuntu.sh

--- 02_apps/deadbeef/ubuntu.sh
+++ 02_apps/deadbeef/ubuntu.sh
@@ -15,8 +15,14 @@ VERSION=0.7.2-2
 ARCH=i386
 [ `uname -m` == x86_64 ] && ARCH=amd64
 wget https://downloads.sourceforge.net/project/deadbeef/debian/deadbeef-static_${VERSION}_${ARCH}.deb -O /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
-dkpg -i /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
+dpkg -i /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
 rm /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
+
+#filebrowser plugin
+wget https://downloads.sourceforge.net/project/deadbeef/plugins/`uname -m`/ddb_filebrowser-1562809-`uname -m`.zip -O /tmp/ddb_filebrowser-1562809-`uname -m`.zip
+unzip -x /tmp/ddb_filebrowser-1562809-x86_64.zip -d /tmp/filebrowser/
+mv /tmp/filebrowser/plugins/ddb_misc_filebrowser_GTK2.so /opt/deadbeef/lib/deadbeef/
+rm -rf /tmp/ddb_filebrowser-1562809-x86_64.zip /tmp/filebrowser/
 
 
 mkdir -p /etc/skel/.config/deadbeef/
