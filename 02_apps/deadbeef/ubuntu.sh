0b7f5478520d2bed09828c39d1deff80079429a7
Antoine Morvan <antoine-morvan@users.noreply.github.com>
2017-03-17 10:12:20 +0100

    Update deadbeef install for ubuntu

--- 02_apps/deadbeef/ubuntu.sh
+++ 02_apps/deadbeef/ubuntu.sh
@@ -6,16 +6,24 @@ export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
 [ ! -e ubuntu_func.sh ] &&  wget -q ${SETUP_SCRIPT_LOCATION}/01_func/ubuntu_func.sh -O ubuntu_func.sh
 source ubuntu_func.sh
 
-add-apt-repository -y ppa:starws-box/deadbeef-player
+#add-apt-repository -y ppa:starws-box/deadbeef-player
+#upgrade
+#install_packs deadbeef
 
-upgrade
-install_packs deadbeef
+VERSION=0.7.2-2
+
+ARCH=i386
+[ `uname -m` == x86_64 ] && ARCH=amd64
+wget https://downloads.sourceforge.net/project/deadbeef/debian/deadbeef-static_${VERSION}_${ARCH}.deb -O /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
+dkpg -i /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
+rm /tmp/deadbeef-static_${VERSION}_${ARCH}.deb
+
 
 mkdir -p /etc/skel/.config/deadbeef/
 retry "wget -q -O /etc/skel/.config/deadbeef/config ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/config"
 
 #add file browser plugin
-wget ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/ddb_misc_filebrowser_GTK2.so -O /usr/lib/deadbeef/ddb_misc_filebrowser_GTK2.so
+#wget ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/ddb_misc_filebrowser_GTK2.so -O /usr/lib/deadbeef/ddb_misc_filebrowser_GTK2.so
 
 
 exit
