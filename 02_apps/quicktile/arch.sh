b4c93d43261358d2a083303cf4c81dade1137bdb
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-01-05 15:14:33 +0100

    (arch) fix quicktile setup

--- 02_apps/quicktile/arch.sh
+++ 02_apps/quicktile/arch.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -eu
 
-exit
-
 #configure script variables
 BG=`cat /setup.dat | sed '1q;d'`
 SETUP_SCRIPT_LOCATION=`cat /setup.dat | sed '2q;d'`
 TESTSYSTEM=`cat /setup.dat | sed '3q;d'`
 INSTALLHEAD=`cat /setup.dat | sed '4q;d'`
 source /arch_func.sh
-
 
 #setup application
 PKGS="python2-six"
@@ -18,20 +15,12 @@ install_packs "$PKGS"
 install_packs_aur "$AURPKGS"
 
 
-# add special script to link XDG config dir to /etc/quicktile
-cat > /usr/local/bin/quicktile_startup.sh << EOF
-#!/bin/bash
-export XDG_CONFIG_HOME=\${HOME}/.config/quicktile/
-/usr/bin/quicktile --daemonize
-EOF
-chmod +x /usr/local/bin/quicktile_startup.sh
-
 mkdir -p /etc/xdg/autostart/
 retry "wget -q -O /etc/xdg/autostart/quicktile.desktop ${SETUP_SCRIPT_LOCATION}/02_apps/quicktile/quicktile.desktop"
 chmod +x /etc/xdg/autostart/quicktile.desktop
 
 mkdir -p /etc/skel/.config/quicktile/
-retry "wget -q -O /etc/skel/.config/quicktile/quicktile.cfg ${SETUP_SCRIPT_LOCATION}/02_apps/quicktile/quicktile.cfg"
+retry "wget -q -O /etc/skel/.config/quicktile.cfg ${SETUP_SCRIPT_LOCATION}/02_apps/quicktile/quicktile.cfg"
 
 exit
 
