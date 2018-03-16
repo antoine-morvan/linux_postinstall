3b4fb3ce022063bd4d35e74dece6ef47fc822916
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-03-16 13:22:46 +0100

    (alpine) fix packages

--- distros/alpine/setup.sh
+++ distros/alpine/setup.sh
@@ -18,9 +18,9 @@ export ENABLE_PAUSE=NO
 export LOGCNT=0
 
 
-#enable all repos
+#enable all online repos
 
-sed -i 's/^#//g' /etc/apk/repositories
+sed -i 's/^#http//g' /etc/apk/repositories
 
 apk update
 apk upgrade
@@ -31,9 +31,9 @@ apk add wget curl
 source alpine_func.sh
 
 
-PACKS="htop bwm-ng  m4 libtool automake autoconf intltool wget bash net-tools zsh samba cifs-utils lshw libtool p7zip htop nethogs iotop parted emacs zip unzip curl fakeroot  fuse cmake  python git screen nmap bzip2 sharutils rsync subversion ttf-dejavu tsocks exfat-utils sshfs  dtach tmux ntfs-3g subversion sdparm hdparm lzip tree cups ghostscript dosfstools intltool  cabextract bwm-ng markdown cloc  tig  openvpn dvtm byobu  vim iptables ruby imagemagick iptraf-ng bash-completion  smartmontools pdftk "
+PACKS="htop bwm-ng  m4 libtool automake autoconf intltool wget bash net-tools zsh samba cifs-utils lshw libtool p7zip htop nethogs iotop parted emacs zip unzip curl fakeroot  fuse cmake  python git screen nmap bzip2 sharutils rsync subversion ttf-dejavu tsocks exfat-utils sshfs  dtach tmux ntfs-3g subversion sdparm hdparm lzip tree cups ghostscript dosfstools intltool  cabextract bwm-ng markdown cloc  tig  openvpn dvtm byobu  vim iptables ruby imagemagick iptraf-ng bash-completion  smartmontools pdftk netcat-openbsd pkgconf unrar rarian "
 
-# arj autotools-dev dnsutils lhasa libomp5 linux-tools-generic lsb-release netcat ntp pkg-config unar unace rar traceroute 
+#  autotools-dev
 UIPACKS="geany qalculate-gtk filezilla vlc alsa-utils elementary-icon-theme pidgin xterm gksu rxvt-unicode lightdm lightdm-gtk-greeter terminator pulseaudio pavucontrol paprefs mate-themes wicd wicd-gtk xfce4 xfce4-goodies xfce4-artwork xfce4-session xfce4-settings xfwm4 xfwm4-themes xfconf thunar numlockx pinta  graphviz gparted filezilla faac libboost-all-dev dbus icoutils zenity hexchat gitg xdot  filelight gdmap qt5-default youtube-dl mcomix unetbootin paman pavumeter xprintidle firefox  arandr  gnome-keyring seahorse python-setuptools tlp  update-manager system-config-printer-common nscd "
 
 if [ "$FASTSETUP" != "YES" ]; then
@@ -42,3 +42,11 @@ if [ "$FASTSETUP" != "YES" ]; then
 fi
 
 install_packs ${PACKS}
+
+
+
+
+
+echo ""
+echo "Done."
+echo ""
