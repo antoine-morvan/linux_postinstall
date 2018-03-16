564d94281bca352d15420d98b801b5555d5ef3d6
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-03-16 13:09:53 +0100

    (alpine) add packages

--- distros/alpine/setup.sh
+++ distros/alpine/setup.sh
@@ -2,7 +2,7 @@
 
 # run as sudo
 
-FASTSETUP=NO
+FASTSETUP=YES
 
 ###
 ### Checks
@@ -17,6 +17,11 @@ export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
 export ENABLE_PAUSE=NO
 export LOGCNT=0
 
+
+#enable all repos
+
+sed -i 's/^#//g' /etc/apk/repositories
+
 apk update
 apk upgrade
 apk add wget curl
@@ -26,3 +31,13 @@ apk add wget curl
 source alpine_func.sh
 
 
+PACKS="htop bwm-ng autotools-dev m4 libtool automake autoconf intltool wget bash net-tools zsh samba cifs-utils lshw libtool p7zip htop nethogs iotop parted emacs zip unzip curl fakeroot linux-tools-generic fuse cmake pkg-config python git screen nmap bzip2 sharutils rsync subversion ttf-dejavu tsocks exfat-utils sshfs ntp dtach tmux ntfs-3g subversion sdparm hdparm dnsutils traceroute lzip tree cups ghostscript dosfstools intltool netcat cabextract bwm-ng markdown cloc arj unar unace tig lhasa openvpn dvtm libomp5 byobu rar vim iptables pidgin xterm gksu rxvt-unicode lightdm lightdm-gtk-greeter terminator pulseaudio pavucontrol paprefs mate-themes wicd wicd-gtk xfce4 xfce4-goodies xfce4-artwork xfce4-session xfce4-settings xfwm4 xfwm4-themes xfconf thunar numlockx pinta ruby imagemagick iptraf-ng arandr elementary-icon-theme gnome-keyring seahorse python-setuptools tlp bash-completion lsb-release smartmontools graphviz gparted filezilla faac libboost-all-dev dbus icoutils zenity hexchat gitg xdot  filelight gdmap qt5-default youtube-dl mcomix unetbootin paman pavumeter xprintidle firefox pdftk update-manager system-config-printer-common nscd "
+
+UIPACKS="geany qalculate-gtk filezilla vlc alsa-utils"
+
+if [ "$FASTSETUP" != "YES" ]; then
+  EXTRAPACKS=" mercurial apache2 lynx php libapache2-mod-php ario audacity keepassx thunderbird ghc clang playonlinux xfburn deluge libreoffice gimp inkscape calibre acetoneiso latex2rtf lyx texmaker pstotext texlive-full pandoc texstudio golang maven gradle openjfx openjfx-source jabref wine handbrake owncloud-client"
+  PACKS+=$EXTRAPACKS
+fi
+
+install_packs ${PACKS}
