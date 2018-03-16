0da05039144013c46af8e3dc32b1d1160869d3fa
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-03-16 13:03:31 +0100

    (alpine) add setup script

---
+++ 01_func/alpine_func.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -eu
+
+# retry the given command until it succeed.
+# $1 : a command as string
+function retry {
+	[ "$LOGCNT" == "" ] && export LOGCNT=0
+	LOGCNT=$((LOGCNT+1))
+	LOGFILE="retry_mainlog_$LOGCNT.log"
+	MSG="Error executing '$1', logging in '$LOGFILE'. Retrying in 5s."
+	$1 >> $LOGFILE 2>> $LOGFILE
+	while [ "$?" != "0" ]; do
+		echo $MSG
+		sleep 5
+    LOGCNT=$((LOGCNT+1))
+		LOGFILE="retry_mainlog_$LOGCNT.log"
+		MSG="Error executing '$1', logging in '$LOGFILE'. Retrying in 5s."
+		$1 >> $LOGFILE 2>> $LOGFILE
+	done
+}
+
+function pause {
+	if [ "$ENABLE_PAUSE" == "YES" ]; then
+		read -p " [ Press Enter ] ... $@"
+	else
+		echo " [ Pause skiped ] ... $@"
+	fi
+}
+
+function upgrade {
+	echo "   Upgrade"
+  apk update
+  apk upgrade
+}
+function install_packs {
+	PACKS=$@
+	echo "   Installing packages [$PACKS]"
+	[ "$PACKS" == "" ] && echo "Warning : package list is empty" && return
+	if [ "$ENABLE_PAUSE" == "YES" ]; then
+		for package in $PACKS; do
+			#pause "pacman --noconfirm -S $package"
+			retry "apk add $package"
+		done
+	else 
+		retry "apk add $PACKS"
+	fi
+}
+
+function dl_and_execute {
+	SCRIPT_LOCATION=$1
+	SCRIPT_FILE=`mktemp --suffix=.sh`
+	pause "dl and execute $1 with arguments ${@:2}"
+	retry "wget --no-cache -q -O ${SCRIPT_FILE} ${SCRIPT_LOCATION}"
+	chmod +x ${SCRIPT_FILE}
+	${SCRIPT_FILE} ${@:2}
+	rm ${SCRIPT_FILE}
+}
---
+++ distros/alpine/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+# run as sudo
+
+FASTSETUP=NO
+
+###
+### Checks
+###
+
+[ `whoami` != root ] && echo "should run as root" && exit 1
+
+
+#configure proxy for installation...
+#test if local server is present
+export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
+export ENABLE_PAUSE=NO
+export LOGCNT=0
+
+apk update
+apk upgrade
+apk add wget curl
+
+#utility functions
+[ ! -e alpine_func.sh ] &&  wget --no-cache -q ${SETUP_SCRIPT_LOCATION}/01_func/alpine_func.sh -O alpine_func.sh
+source alpine_func.sh
+
+
