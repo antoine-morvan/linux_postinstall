83cf9432115be2a11b0d2cbfbfde423dcbc3ced4
Antoine Morvan <antoine-morvan@users.noreply.github.com>
2018-05-21 12:46:23 +0200

    Update ubuntu.sh

--- 02_apps/terminator/ubuntu.sh
+++ 02_apps/terminator/ubuntu.sh
@@ -2,7 +2,7 @@
 
 #configure script variables
 [ `whoami` != root ] && echo "should run as root" && exit 1
-export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
+export SETUP_SCRIPT_LOCATION=http://koub.org/linux/
 [ ! -e ubuntu_func.sh ] &&  wget -q ${SETUP_SCRIPT_LOCATION}/01_func/ubuntu_func.sh -O ubuntu_func.sh
 source ubuntu_func.sh
 
