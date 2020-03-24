8ef1de03f440821b7fdfca8207a35ced45f251fd
Antoine Morvan <antoine.morvan.pro@gmail.com>
2020-03-24 22:39:00 +0100

    fixup

--- 02_apps/quicktile/ubuntu.sh
+++ 02_apps/quicktile/ubuntu.sh
@@ -8,9 +8,9 @@ export SETUP_SCRIPT_LOCATION=https://raw.githubusercontent.com/antoine-morvan/li
 source ubuntu_func.sh
 
 #setup application deps
-apt install -y apt-get install python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
+apt install -y python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
 
-sudo -H pip3 install https://github.com/ssokolow/quicktile/archive/master.zip
+pip3 install https://github.com/ssokolow/quicktile/archive/master.zip
 
 #add default config
 mkdir -p /etc/skel/.config/
