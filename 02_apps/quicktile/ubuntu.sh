1558f1717b0a1b1fe1ab9d5f9a1dd9a7e64a38ef
Antoine Morvan <antoine.morvan.pro@gmail.com>
2020-03-24 20:31:46 +0100

    Fix dependencies for quicktile

--- 02_apps/quicktile/ubuntu.sh
+++ 02_apps/quicktile/ubuntu.sh
@@ -8,9 +8,9 @@ export SETUP_SCRIPT_LOCATION=https://raw.githubusercontent.com/antoine-morvan/li
 source ubuntu_func.sh
 
 #setup application deps
-apt-get -y install python python-gtk2 python-xlib python-dbus python-setuptools git libpango1.0-0 python-pip python-wnck
+apt install -y apt-get install python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
 
-sudo -H pip2 install https://github.com/ssokolow/quicktile/archive/master.zip
+sudo -H pip3 install https://github.com/ssokolow/quicktile/archive/master.zip
 
 #add default config
 mkdir -p /etc/skel/.config/
