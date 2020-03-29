ccb7227c60c0ab4e82db2b15f66e9c933690498c
Antoine Morvan <antoine.morvan.pro@gmail.com>
2020-03-29 16:45:52 +0200

    (Ubuntu) Add docker setup

---
+++ 02_apps/docker/ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+#configure script variables
+[ `whoami` != root ] && echo "should run as root" && exit 1
+#export SETUP_SCRIPT_LOCATION=http://koub.org/files/linux/
+export SETUP_SCRIPT_LOCATION=https://raw.githubusercontent.com/antoine-morvan/linux_postinstall/master/
+[ ! -e ubuntu_func.sh ] &&  wget -q ${SETUP_SCRIPT_LOCATION}/01_func/ubuntu_func.sh -O ubuntu_func.sh
+source ubuntu_func.sh
+
+#install dep
+
+apt remove -y docker docker-engine docker.io containerd runc
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt update
+
+apt install -y docker-ce docker-ce-cli containerd.io
+
+pip3 install docker-compose
+
+usermod -aG docker $USR
+
+exit 0
+
--- distros/ubuntu/ubuntu.sh
+++ distros/ubuntu/ubuntu.sh
@@ -124,6 +124,8 @@ dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/truecrypt/ubuntu.sh
 dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/gen-eclipse/ubuntu.sh
 #dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/purple-plugins/ubuntu.sh
 
+dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/docker/ubuntu.sh
+
 dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/deadbeef/ubuntu.sh
 dl_and_execute ${SETUP_SCRIPT_LOCATION}/02_apps/skype/ubuntu.sh
 
