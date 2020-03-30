a08acac6686355a390278c7192fff18f59a9139e
Antoine Morvan <antoine.morvan.pro@gmail.com>
2020-03-30 10:18:08 +0200

    (Ubuntu) Remove gdm/ubuntu session in the end

--- distros/ubuntu/ubuntu.sh
+++ distros/ubuntu/ubuntu.sh
@@ -89,14 +89,13 @@ EOF
 chmod +x /usr/local/bin/gitstorecredential-10h
 
 
-#remove amazon app. FFS
-apt -y remove unity-webapps-common gdm3 ubuntu-session
+
 #ttf-mscorefonts
 PACKS="htop geany bwm-ng qalculate-gtk filezilla vlc apt-file autotools-dev m4 libtool automake autoconf intltool wget bash net-tools zsh samba cifs-utils lshw libtool p7zip htop nethogs iotop parted emacs zip unzip curl fakeroot alsa-utils linux-tools-generic fuse cmake pkg-config python git screen nmap bzip2 sharutils rsync subversion ttf-dejavu tsocks exfat-utils sshfs ntp dtach tmux ntfs-3g subversion sdparm hdparm dnsutils traceroute lzip tree cups ghostscript dosfstools intltool netcat cabextract bwm-ng markdown cloc arj unar unace tig lhasa openvpn dvtm libomp5 byobu rar vim iptables pidgin xterm rxvt-unicode lightdm lightdm-gtk-greeter terminator pulseaudio pavucontrol paprefs mate-themes xfce4 xfce4-goodies xfce4-session xfce4-settings xfwm4 xfwm4-themes xfconf thunar numlockx pinta ruby imagemagick iptraf-ng arandr elementary-icon-theme gnome-keyring seahorse python-setuptools tlp bash-completion lsb-release smartmontools graphviz gparted filezilla faac libboost-all-dev dbus icoutils zenity hexchat gitg xdot  filelight gdmap qt5-default youtube-dl pasystray pavumeter xprintidle firefox update-manager system-config-printer-common nscd apt-transport-https ca-certificates curl gnupg-agent software-properties-common "
 
 
 #install various tools
-apt install -y --no-install-recommends $PACKS
+apt install -y --no-install-recommends -q -o Dpkg::Options::="--force-confdef" $PACKS
 
 if [ "$FASTSETUP" != "YES" ]; then
   EXTRAPACKS=" mercurial lynx audacity keepassx thunderbird ghc clang playonlinux xfburn deluge libreoffice gimp inkscape calibre acetoneiso latex2rtf lyx texmaker pstotext texlive-full pandoc texstudio golang maven gradle openjfx openjfx-source jabref handbrake owncloud-client "
@@ -149,6 +148,7 @@ chown -R $USR:$USR /home/$USR
 
 
 #clean
+apt -y remove unity-webapps-common gdm3 ubuntu-session
 rm -f retry_* ubuntu_func.sh
-
-exit
+reboot
+exit 0
