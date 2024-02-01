b3ce3354a4c877c562f9d7a140f8a2681712b291
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-02-01 22:47:58 +0100

    Fix docker setup; remove todo item

--- distros/debian12GW.sh
+++ distros/debian12GW.sh
@@ -3,7 +3,6 @@ set -eu -o pipefail
 
 ###################################################################################
 ##### TODO
-##  * Fix dhclient config for router ...
 ##  * cleanup squid config
 ##  * add mechanism to prepare a list of local hosts from external file
 ###################################################################################
@@ -91,7 +90,7 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ###########################
 ##### Setup Users
