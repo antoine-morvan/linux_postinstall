8f0e5e8828e15479415972a4ef025df7fccfa6c0
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-02-04 16:21:05 +0100

    enable docker setup

--- distros/debian12GW.sh
+++ distros/debian12GW.sh
@@ -84,18 +84,18 @@ apt install -y bind9 isc-dhcp-server squid ipcalc bwm-ng iptraf nethogs byobu su
 
 ## Warning: docker setup seems to produce unbootable VM...
 
-# echo " -- Install docker"
-# # from https://docs.docker.com/engine/install/debian/
-# install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-# chmod a+r /etc/apt/keyrings/docker.asc
+echo " -- Install docker"
+# from https://docs.docker.com/engine/install/debian/
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-# # Add the repository to Apt sources:
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
-# apt-get update
-# apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# GROUPS+=",docker"
+# Add the repository to Apt sources:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+GROUPS+=",docker"
 
 ###########################
 ##### Setup Users
