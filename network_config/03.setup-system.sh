82a4c8fc094fdcf8067ea39840bae33e7b5b78f1
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-11-06 13:16:11 +0100

    rename file

---
+++ network_config/03.setup-system.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+# Check that we can source config
+source config.sh
+
+case $ID_LIKE in
+    *debian*|*ubuntu*)
+        usermod -aG sudo ${SUDOUSER}
+        ;;
+    *fedora*|*rhel*)
+        usermod -aG wheel ${SUDOUSER}
+        ;;
+esac
+
+## set lan iface IP
+case $GEN_CONFIG in
+  YES) INTERFACES_FILE=./interfaces             ;;
+  *)   INTERFACES_FILE=/etc/network/interfaces  ;;
+esac
+cat >> ${INTERFACES_FILE} << EOF
+auto ${WEBIFACE}
+allow-hotplug ${WEBIFACE}
+iface ${WEBIFACE} inet dhcp
+
+auto ${LANIFACE}
+iface ${LANIFACE} inet static
+  address ${SERVERLANIP}
+  netmask ${LANMASK}
+  network ${LANNETADDRESS}
+  broadcast ${LANBROADCAST}
+EOF
+
+## Set dhclient to use proper domain & name server
+case $GEN_CONFIG in
+  YES) DHCLIENT_FILE=./dhclient.conf          ;;
+  *)   DHCLIENT_FILE=/etc/dhcp/dhclient.conf  ;;
+esac
+cat >> ${DHCLIENT_FILE} << EOF
+supersede domain-name "$DOMAIN_NAME";
+prepend domain-name-servers 127.0.0.1;
+EOF
+
+exit 0
