a9456e9cdce6e46588865b842f0d0cdc245856a5
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-11-06 13:20:29 +0100

    Copy content in services config generation

--- network_config/04.generate.dhcp.sh
+++ network_config/04.generate.dhcp.sh
@@ -4,28 +4,65 @@ set -eu -o pipefail
 # Check that we can source config
 source config.sh
 
-SUDOUSER=${SUDOUSER:-admin}
+[ -f config.fixed_hosts.list ] && FIXED_IPS=$(cat config.fixed_hosts.list \
+  | sed -r 's/#.*//g' | sed -r 's/\s+$//g' | grep -v "^#\|^\s*$" \
+  | sed -r 's/\s+/:/g' | sed 's/\r/\n/g')
+FIXED_IPS=${FIXED_IPS:-""}
 
 
-set +e
-cat /etc/passwd | grep ^${SUDOUSER}: &> /dev/null
-RES=$?
-set -e
-if [ $RES != 0 ]; then
-    useradd \
-        -d /home/${SUDOUSER} \
-        -m \
-        ${SUDOUSER}
 
-fi
-
-case $ID_LIKE in
-    *debian*|*ubuntu*)
-        usermod -aG sudo ${SUDOUSER}
-        ;;
-    *fedora*|*rhel*)
-        usermod -aG wheel ${SUDOUSER}
-        ;;
+case $GEN_CONFIG in
+  YES) 
+    DHCPD_FILE=./dhcpd.conf
+    DHCPD_DEFAULT=./isc-dhcp-server
+    ;;
+  *)   
+    DHCPD_FILE=/etc/dhcp/dhcpd.conf
+    DHCPD_DEFAULT=/etc/default/isc-dhcp-server
+    ;;
 esac
 
-exit 0
+case $GEN_CONFIG in
+  YES)
+    cat > ${DHCPD_DEFAULT} << EOF
+INTERFACESv4="${LANIFACE}"
+EOF
+    cat > ${DHCPD_FILE} << EOF
+option domain-name ${DOMAIN_NAME};
+option domain-name-servers ${SERVERLANIP};
+authoritative;
+EOF
+    ;;
+  *)
+    ## enable DHCPd on lan iface
+    sed -i -r "s/^(INTERFACESv4=).*/\1\"${LANIFACE}\"/g" ${DHCPD_DEFAULT}
+
+    ## set DHCPd config
+    sed -i -r "s/^(option domain-name )(.*)/\1${DOMAIN_NAME};/g" ${DHCPD_FILE}
+    sed -i -r "s/^(option domain-name-servers )(.*)/\1${SERVERLANIP};/g" ${DHCPD_FILE}
+    sed -i -r "s/^#authoritative;/authoritative;/g" ${DHCPD_FILE}
+    ;;
+esac
+
+
+cat >> ${DHCPD_FILE} << EOF
+subnet ${LANNETADDRESS} netmask ${LANMASK} {
+  range ${DHCPRANGESTARTIP} ${DHCPRANGESTOPIP};
+  option routers ${SERVERLANIP};
+}
+
+EOF
+
+for FixedIP in $FIXED_IPS; do
+  MAC=$(echo $FixedIP | rev | cut -d':' -f3- | rev )
+  IP=$(echo $FixedIP | rev | cut -d':' -f2 | rev )
+  NAME=$(echo $FixedIP | rev | cut -d':' -f1 | rev )
+  cat >> ${DHCPD_FILE} << EOF
+host $NAME {
+        hardware ethernet $MAC;
+        fixed-address $IP;
+        option dhcp-client-identifier "$NAME";
+        option host-name "$NAME";
+}
+EOF
+done
--- network_config/05.generate.dns.sh
+++ network_config/05.generate.dns.sh
@@ -4,28 +4,121 @@ set -eu -o pipefail
 # Check that we can source config
 source config.sh
 
-SUDOUSER=${SUDOUSER:-admin}
 
+echo "[INFO] Setup DNS"
+## setup DNS
 
-set +e
-cat /etc/passwd | grep ^${SUDOUSER}: &> /dev/null
-RES=$?
-set -e
-if [ $RES != 0 ]; then
-    useradd \
-        -d /home/${SUDOUSER} \
-        -m \
-        ${SUDOUSER}
-
-fi
-
-case $ID_LIKE in
-    *debian*|*ubuntu*)
-        usermod -aG sudo ${SUDOUSER}
-        ;;
-    *fedora*|*rhel*)
-        usermod -aG wheel ${SUDOUSER}
-        ;;
+case $GEN_CONFIG in
+  YES) BIND_FOLDER=.          ;;
+  *)   BIND_FOLDER=/etc/bind/ ;;
 esac
 
-exit 0
+cat > ${BIND_FOLDER}/named.conf.options << EOF
+options {
+  directory "/var/cache/bind";
+
+  recursion yes;
+  allow-query { any; };
+  allow-recursion { any; };
+  allow-query-cache { any; };
+
+  forwarders {
+EOF
+
+for dns in $EXTERNALDNSLIST; do
+cat >> ${BIND_FOLDER}/named.conf.options << EOF
+    $dns;
+EOF
+done
+cat >> ${BIND_FOLDER}/named.conf.options << EOF
+  };
+
+  dnssec-validation auto;
+
+  auth-nxdomain no;
+  listen-on-v6 { any; };
+  listen-on { any; };
+};
+EOF
+
+cat >> ${BIND_FOLDER}/named.conf.local  << EOF
+
+zone "${DOMAIN_NAME}" {
+    type master;
+    file "${BIND_FOLDER}/db.${DOMAIN_NAME}";
+};
+
+EOF
+cat >> ${BIND_FOLDER}/db.${DOMAIN_NAME} << EOF
+\$TTL    604800
+@       IN      SOA     $HOSTNAME.$DOMAIN_NAME. root.$HOSTNAME.$DOMAIN_NAME. (
+                              2         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
+;
+@       IN      NS      $HOSTNAME.$DOMAIN_NAME.
+@       IN      A       127.0.0.1
+
+localhost.$DOMAIN_NAME. IN A 127.0.0.1
+$HOSTNAME.$DOMAIN_NAME. IN A ${SERVERLANIP}
+;your sites
+EOF
+
+ZONES=""
+# Add reverse for router
+IP_ZONE=$(echo $SERVERLANIP |cut -d'.' -f-3)
+LAST_DIGIT=$(echo $SERVERLANIP |cut -d'.' -f4)
+ZONE_FILE=${BIND_FOLDER}/db.$IP_ZONE
+if [ ! -f $ZONE_FILE ]; then
+  ZONES+=" $IP_ZONE"
+  cat > $ZONE_FILE << EOF
+\$TTL    604800
+@       IN      SOA     $HOSTNAME.$DOMAIN_NAME. root.$HOSTNAME.$DOMAIN_NAME. (
+                            2         ; Serial
+                        604800         ; Refresh
+                        86400         ; Retry
+                      2419200         ; Expire
+                        604800 )       ; Negative Cache TTL
+      NS      $HOSTNAME.$DOMAIN_NAME.
+EOF
+fi
+echo "$LAST_DIGIT PTR $HOSTNAME.$DOMAIN_NAME." >> $ZONE_FILE
+
+# Add reverse for fixed hosts
+for FixedIP in $FIXED_IPS; do
+  MAC=$(echo $FixedIP | rev | cut -d':' -f3- | rev )
+  IP=$(echo $FixedIP | rev | cut -d':' -f2 | rev )
+  NAME=$(echo $FixedIP | rev | cut -d':' -f1 | rev )
+  echo "$NAME.$DOMAIN_NAME. IN A $IP" >> ${BIND_FOLDER}/db.${DOMAIN_NAME}
+
+  IP_ZONE=$(echo $IP |cut -d'.' -f-3)
+  LAST_DIGIT=$(echo $IP |cut -d'.' -f4)
+  ZONE_FILE=${BIND_FOLDER}/db.$IP_ZONE
+  if [ ! -f $ZONE_FILE ]; then
+    ZONES+=" $IP_ZONE"
+    cat > $ZONE_FILE << EOF
+\$TTL    604800
+@       IN      SOA     $HOSTNAME.$DOMAIN_NAME. root.$HOSTNAME.$DOMAIN_NAME. (
+                              2         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
+        NS      $HOSTNAME.$DOMAIN_NAME.
+EOF
+  fi
+  echo "$LAST_DIGIT PTR $NAME.$DOMAIN_NAME." >> $ZONE_FILE
+done
+
+for ZONE in $ZONES; do
+  ZONE_FILE=${BIND_FOLDER}/db.$IP_ZONE
+  REV_ZONE=$(echo $ZONE | cut -d'.' -f3).$(echo $ZONE | cut -d'.' -f2).$(echo $ZONE | cut -d'.' -f1)
+  cat >> ${BIND_FOLDER}/named.conf.local  << EOF
+zone "${REV_ZONE}.in-addr.arpa" {
+    type master;
+    file "${ZONE_FILE}";
+};
+EOF
+done
