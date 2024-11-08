15f988dc9ba0c0de73a2f14b79999b52fca2d1de
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-11-08 22:31:25 +0100

    First isntall script proto

--- network_config/08.install.sh
+++ network_config/08.install.sh
@@ -1,4 +1,40 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
 
-case ${ID_LIKE:-${ID}} in
+############################################################################################
+## Load config
+############################################################################################
 
-esac
+# [ "$(whoami)" != root ] && echo "[NETCONF] ERROR: must run as root" && exit 1
+source config.sh
+
+
+FIREWALL_FOLDER=./gen.firewall
+BIND_FOLDER=./gen.bind
+DHCP_FOLDER=./gen.dhcp
+
+TARGET_DHCP=/etc/dhcp/
+TARGET_BIND=/etc/bind/
+TARGET_FIREWALL=/usr/
+
+[ -d $DHCP_FOLDER ] && (
+    DHCPD_FILE=/etc/dhcp/dhcpd.conf
+    DHCPD_DEFAULT=/etc/default/isc-dhcp-server
+
+    cp $DHCP_FOLDER/dhcpd.conf ${DHCPD_FILE}
+    cp $DHCP_FOLDER/isc-dhcp-server ${DHCPD_DEFAULT}
+)
+[ -d $BIND_FOLDER ] && (
+    cp $BIND_FOLDER/named.conf.options ${TARGET_BIND}
+    cp $BIND_FOLDER/named.conf.local ${TARGET_BIND}
+    cp $BIND_FOLDER/db.${DOMAIN_NAME} ${TARGET_BIND}
+    cp $BIND_FOLDER/db.[0-9]* ${TARGET_BIND}
+)
+[ -d $FIREWALL_FOLDER ] && (
+    cp $FIREWALL_FOLDER $TARGET_FIREWALL
+)
+
+############################################################################################
+## Done
+############################################################################################
+exit 0
