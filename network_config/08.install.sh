e6b8b017a1e2798472985cd9c7a1b2ce809c86c8
Antoine Morvan <antoine.morvan.phd@gmail.com>
2024-11-09 14:40:37 +0100

    Add logging

--- network_config/08.install.sh
+++ network_config/08.install.sh
@@ -7,6 +7,7 @@ set -eu -o pipefail
 
 # [ "$(whoami)" != root ] && echo "[NETCONF] ERROR: must run as root" && exit 1
 source config.sh
+echo "[NETCONF] INFO: Install files"
 
 FIREWALL_FOLDER=./gen.firewall
 BIND_FOLDER=./gen.bind
@@ -16,6 +17,7 @@ TARGET_DHCP=/etc/dhcp/
 TARGET_BIND=/etc/bind/
 TARGET_FIREWALL=/usr/
 
+echo "[NETCONF] INFO:  - DHCP"
 [ -d $DHCP_FOLDER ] && (
     DHCPD_FILE=/etc/dhcp/dhcpd.conf
     DHCPD_DEFAULT=/etc/default/isc-dhcp-server
@@ -23,18 +25,21 @@ TARGET_FIREWALL=/usr/
     cp $DHCP_FOLDER/dhcpd.conf ${DHCPD_FILE}
     cp $DHCP_FOLDER/isc-dhcp-server ${DHCPD_DEFAULT}
 )
+echo "[NETCONF] INFO:  - DNS"
 [ -d $BIND_FOLDER ] && (
     cp $BIND_FOLDER/named.conf.options ${TARGET_BIND}
     cp $BIND_FOLDER/named.conf.local ${TARGET_BIND}
     cp $BIND_FOLDER/db.${DOMAIN_NAME} ${TARGET_BIND}
     cp $BIND_FOLDER/db.[0-9]* ${TARGET_BIND}
 )
+echo "[NETCONF] INFO:  - Firewall"
 [ -d $FIREWALL_FOLDER ] && (
     cp -R $FIREWALL_FOLDER/* $TARGET_FIREWALL
     chmod +x ${TARGET_FIREWALL}/sbin/firewall_router.*.sh
 )
 
 ############################################################################################
-## Done
+## Exit
 ############################################################################################
+echo "[NETCONF] INFO: Install files: Done."
 exit 0
