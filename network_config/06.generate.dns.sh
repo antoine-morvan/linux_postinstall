c5d22a8fc35120e289402413c84782e5ba5da5f3
Antoine Morvan <antoine.morvan.phd@gmail.com>
2025-05-11 21:30:53 +0000

    Update DNS gen
    - Add option to use host DNS;
    - Add disabled logging section

--- network_config/06.generate.dns.sh
+++ network_config/06.generate.dns.sh
@@ -40,6 +40,14 @@ options {
   forwarders {
 EOF
 
+HOST_DNS_LIST=$(cat /etc/resolv.conf | grep nameserver| sed -r 's/nameserver\s+//g' | grep -v '^127.' | xargs)
+case ${USE_HOST_DNS:-NO} in
+  YES|APPEND) DNS_LIST="${DNS_LIST} ${HOST_DNS_LIST}" ;;
+  PREPEND)    DNS_LIST="${HOST_DNS_LIST} ${DNS_LIST}" ;;
+  ONLY)       DNS_LIST="${HOST_DNS_LIST}" ;;
+  *) : ;; # use dns list from config
+esac
+
 for dns in $DNS_LIST; do
 cat >> ${BIND_FOLDER}/named.conf.options << EOF
     $dns;
@@ -55,6 +63,156 @@ cat >> ${BIND_FOLDER}/named.conf.options << EOF
   listen-on { any; };
 };
 EOF
+
+# disabled until further testing
+false && (
+  cat >> ${BIND_FOLDER}/named.conf.options << EOF
+logging {
+    channel default_file {
+        file "/var/log/named/default.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel general_file {
+        file "/var/log/named/general.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel database_file {
+        file "/var/log/named/database.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel security_file {
+        file "/var/log/named/security.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel config_file {
+        file "/var/log/named/config.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel resolver_file {
+        file "/var/log/named/resolver.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel xfer-in_file {
+        file "/var/log/named/xfer-in.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel xfer-out_file {
+        file "/var/log/named/xfer-out.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel notify_file {
+        file "/var/log/named/notify.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel client_file {
+        file "/var/log/named/client.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel unmatched_file {
+        file "/var/log/named/unmatched.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel queries_file {
+        file "/var/log/named/queries.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel network_file {
+        file "/var/log/named/network.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel update_file {
+        file "/var/log/named/update.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel dispatch_file {
+        file "/var/log/named/dispatch.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel dnssec_file {
+        file "/var/log/named/dnssec.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+    channel lame-servers_file {
+        file "/var/log/named/lame-servers.log" versions 3 size 5m;
+        severity dynamic;
+        print-time yes;
+        print-category yes;
+        print-severity yes;
+    };
+
+    category client { client_file; };
+    category config { config_file; };
+    category default { default_file; };
+    category general { general_file; };
+    category database { database_file; };
+    category security { security_file; };
+    category resolver { resolver_file; };
+    category xfer-in { xfer-in_file; };
+    category xfer-out { xfer-out_file; };
+    category notify { notify_file; };
+    category unmatched { unmatched_file; };
+    category queries { queries_file; };
+    category network { network_file; };
+    category update { update_file; };
+    category dispatch { dispatch_file; };
+    category dnssec { dnssec_file; };
+    category lame-servers { lame-servers_file; };
+    category rate-limit { general_file; };
+    category rpz { general_file; };
+    category update-security { update_file; };
+};
+EOF
+  mkdir -p /var/log/named/
+  chmod a+rwx /var/log/named/
+)
 
 ###########################
 ## db.domain init
